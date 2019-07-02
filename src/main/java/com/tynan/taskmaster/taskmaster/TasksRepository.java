3a98840d38dc8c156ab877d3fba4768aa68dc134
Peter Tynan <pettynan@amazon.com>
2019-07-02 13:24:22 -0700

    first step, matching the demo

---
+++ src/main/java/com/tynan/taskmaster/taskmaster/TasksRepository.java
@@ -0,0 +1,11 @@
+package com.tynan.taskmaster.taskmaster;
+
+import org.socialsignin.spring.data.dynamodb.repository.EnableScan;
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+@EnableScan
+public interface TasksRepository extends CrudRepository<Tasks, String> {
+    Optional<Tasks> findById(String id);
+}
